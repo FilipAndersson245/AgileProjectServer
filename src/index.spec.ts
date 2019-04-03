c322e36c15bdeda02d52e74300fd62fdbacd671a
FilipAndersson245 <jabos123@gmail.com>
2019-04-03 23:47:03 +0200

    Added test suite for gantry post.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,59 @@
-it("example", () => {
-  expect(1 === 1);
+import app from "./index";
+import request from "supertest";
+import {
+  gantryDoesNotExistResponse,
+  unauthorizationResponse
+} from "./models/error";
+
+afterEach(() => {
+  app.close();
+});
+
+const goodId = "abc123";
+const badId = "jkl345";
+
+const goodBody = {
+  position: [3.213134, 12.438324]
+};
+const token = "fhsakdjhjkfds";
+
+describe("Update gantry position test", () => {
+  test("Should send data correctly", async () => {
+    const response = await request(app)
+      .post(`/gantries/${goodId}`)
+      .auth(token, { type: "bearer" })
+      .send(goodBody);
+
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual("application/json");
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        id: expect.stringMatching(goodId),
+        position: expect.any(Object),
+        lastUpdated: expect.any(Number),
+        price: expect.any(Number)
+      })
+    );
+  });
+
+  test("Should send to non-existing gantry", async () => {
+    const response = await request(app)
+      .post(`/gantries/${badId}`)
+      .auth(token, { type: "bearer" })
+      .send(goodBody);
+
+    expect(response.status).toEqual(404);
+    expect(response.type).toEqual("application/json");
+    expect(response.body).toEqual(gantryDoesNotExistResponse);
+  });
+
+  test("Should be unauthorized", async () => {
+    const response = await request(app)
+      .post(`/gantries/${goodId}`)
+      .auth("jhkfdskjhfsdfsdih", { type: "bearer" })
+      .send(goodBody);
+    expect(response.status).toEqual(401);
+    expect(response.type).toEqual("application/json");
+    expect(response.body).toEqual(unauthorizationResponse);
+  });
 });
